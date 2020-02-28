8b2ffcf5b17ff4d57b2e7e455e6f6dddb6d771c6
Yogaswara Budiman <yoga99220@gmail.com>
2020-02-27 22:34:49 -0700

    Updates

---
+++ precache-manifest.c56fc5ef2de8d126fc8a74d01a11525d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de47737f56c333e2d6008a06e791e1fe",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/index.html"
+  },
+  {
+    "revision": "7b4942cf75667e31a87e",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/static/css/main.4f041620.chunk.css"
+  },
+  {
+    "revision": "6ae79991e377e50b81a6",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/static/js/2.87a54e3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/static/js/2.87a54e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4942cf75667e31a87e",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/static/js/main.90272062.chunk.js"
+  },
+  {
+    "revision": "ed56224b46bdba64ba17",
+    "url": "/YogaBudiman/pathfinderVisualizer.git/static/js/runtime-main.ff0f7511.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/YogaBudiman/pathfinderVisualizer.git/precache-manifest.c56fc5ef2de8d126fc8a74d01a11525d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YogaBudiman/pathfinderVisualizer.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
